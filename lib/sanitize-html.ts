bedea13d7560c21b061bd5d3c92294cfabc91520
Daniel Kozhemiakin <daniel.kojemyakin@icloud.com>
2026-01-02 14:32:03 +0300

    Fix TypeScript error in sanitizeHTML function

--- lib/sanitize-html.ts
+++ lib/sanitize-html.ts
@@ -1,6 +1,16 @@
 import DOMPurify from 'dompurify';
 import { JSDOM } from 'jsdom';
 
+const sanitizeConfig = {
+  ALLOWED_TAGS: [
+    'p', 'br', 'strong', 'em', 'u', 's', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'ul', 'ol', 'li', 'a', 'img', 'blockquote', 'code', 'pre', 'div', 'span',
+    'table', 'thead', 'tbody', 'tr', 'th', 'td', 'hr'
+  ],
+  ALLOWED_ATTR: ['href', 'src', 'alt', 'title', 'class', 'id', 'target', 'rel'],
+  ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
+};
+
 /**
  * Санитизация HTML контента для безопасного отображения
  * Используется для контента, который рендерится через dangerouslySetInnerHTML
@@ -8,28 +18,12 @@ import { JSDOM } from 'jsdom';
 export function sanitizeHTML(html: string): string {
   // Для серверного рендеринга используем JSDOM
   if (typeof window === 'undefined') {
-    const window = new JSDOM('').window;
-    const purify = DOMPurify(window as unknown as Window);
-    return purify.sanitize(html, {
-      ALLOWED_TAGS: [
-        'p', 'br', 'strong', 'em', 'u', 's', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-        'ul', 'ol', 'li', 'a', 'img', 'blockquote', 'code', 'pre', 'div', 'span',
-        'table', 'thead', 'tbody', 'tr', 'th', 'td', 'hr'
-      ],
-      ALLOWED_ATTR: ['href', 'src', 'alt', 'title', 'class', 'id', 'target', 'rel'],
-      ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
-    });
+    const dom = new JSDOM('');
+    const purify = DOMPurify(dom.window as unknown as Window & typeof globalThis);
+    return purify.sanitize(html, sanitizeConfig);
   }
 
   // Для клиентского рендеринга
-  return DOMPurify.sanitize(html, {
-    ALLOWED_TAGS: [
-      'p', 'br', 'strong', 'em', 'u', 's', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-      'ul', 'ol', 'li', 'a', 'img', 'blockquote', 'code', 'pre', 'div', 'span',
-      'table', 'thead', 'tbody', 'tr', 'th', 'td', 'hr'
-    ],
-    ALLOWED_ATTR: ['href', 'src', 'alt', 'title', 'class', 'id', 'target', 'rel'],
-    ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
-  });
+  return DOMPurify.sanitize(html, sanitizeConfig);
 }
 
